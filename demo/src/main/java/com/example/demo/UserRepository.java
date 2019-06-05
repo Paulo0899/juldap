7dd48ebd56e695bbd61b08b2e64fed1060177896
Paulo Ricardo <Paulo0899@gmail.com>
2019-06-05 17:17:56 -0300

    cm

---
+++ demo/src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * UserRepository
+ */
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    
+}
